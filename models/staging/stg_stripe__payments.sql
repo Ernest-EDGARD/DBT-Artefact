e0882c0c6dc25793e805c3ddcf60c358f8456873
ernest.edgard@gmail.com <ernest.edgard@gmail.com>
2026-01-07 15:33:13 +0000

    c

---
+++ models/staging/stg_stripe__payments.sql
@@ -0,0 +1,9 @@
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- dbt gère souvent les montants en centimes, on divise par 100 pour avoir des dollars
+    amount / 100 as amount,
+    created as created_at
+from {{ source('raw', 'payments') }}
