d054e614aa2fa23fdc45fdc50988913da344ced6
ernest.edgard@gmail.com <ernest.edgard@gmail.com>
2026-01-07 16:10:28 +0000

    c

--- models/staging/stg_stripe__payments.sql
+++ models/staging/stg_stripe__payments.sql
@@ -6,4 +6,4 @@ select
     -- dbt gère souvent les montants en centimes, on divise par 100 pour avoir des dollars
     amount / 100 as amount,
     created as created_at
-from dbt-artefact.jaffle_shop.payments
+from dbt-tutorial.jaffle_shop.payments
