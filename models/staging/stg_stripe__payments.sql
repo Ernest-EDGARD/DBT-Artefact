f9084157490009c9c5de28302c53a066fa695f73
ernest.edgard@gmail.com <ernest.edgard@gmail.com>
2026-01-07 16:13:26 +0000

    c

--- models/staging/stg_stripe__payments.sql
+++ models/staging/stg_stripe__payments.sql
@@ -1,9 +1,7 @@
-select
-    id as payment_id,
+select id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    -- dbt gère souvent les montants en centimes, on divise par 100 pour avoir des dollars
     amount / 100 as amount,
     created as created_at
 from dbt-tutorial.jaffle_shop.payments
