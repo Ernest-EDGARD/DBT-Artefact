e870e1f50a15930717ddb69e12eb50714f7fba43
Ernest EDGARD <ernest.edgard@gmail.com>
2026-01-08 16:28:17 +0000

    c

--- models/staging/Carrefour/stg_carrefour_sales.sql
+++ models/staging/Carrefour/stg_carrefour_sales.sql
@@ -1,11 +1,11 @@
 WITH prx AS (
-    {{ get_carrefour_sales_macro('crf_prx_sales_fr_data') }}
+    {{ carrefour_sales_macro('crf_prx_sales_fr_data') }}
 ),
 sup AS (
-    {{ get_carrefour_sales_macro('crf_sup_sales_fr_data') }}
+    {{ carrefour_sales_macro('crf_sup_sales_fr_data') }}
 ),
 hyp AS (
-    {{ get_carrefour_sales_macro('crf_hyp_sales_fr_data') }}
+    {{ carrefour_sales_macro('crf_hyp_sales_fr_data') }}
 )
 
 SELECT * FROM prx
